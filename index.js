c04733aaa3c4b0b0b1f9b1047c0b5f10be30f864
louisinayinde <louisinayinde@gmail.com>
2024-04-05 18:30:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,76 +26,43 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const originalUrls = []
+const shortUrls = []
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  let url = req.body.url;
-  // Validate the URL
-  try {
-    urlObj = new URL(url);
-    // Validate DNS Domain
-    dns.lookup(urlObj.hostname, (err, address, family) => {
-      // If the DNS domain does not exist no address returned
-      if (!address){
-        res.json({ error: 'invalid url' })
-      } 
-      // We have a valid URL!
-      else {
-        let original_url = urlObj.href;
-        // Check that URL does not already exist in database 
-        URLModel.findOne({original_url: original_url}).then(
-          (foundURL) => {
-            if (foundURL) {
-              res.json(
-                {
-                  original_url: foundURL.original_url,
-                  short_url: foundURL.short_url
-                })
-          } 
-          // If URL does not exist create a new short url and 
-          // add it to the database
-          else {
-            let short_url = 1;
-            // Get the latest short_url
-            URLModel.find({}).sort(
-              {short_url: "desc"}).limit(1).then(
-                (latestURL) => {
-                if (latestURL.length > 0){
-                  // Increment the latest short url by adding 1
-                  short_url = parseInt(latestURL[0].short_url) + 1;
-                }
-                resObj = {
-                  original_url: original_url,
-                  short_url: short_url
-                }
-                
-                // Create an entry in the database
-                let newURL = new URLModel(resObj);
-                newURL.save();
-                res.json(resObj);
-                }
-            )
-          }
-        })
-      }
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+
+  if(!url.includes("https://") && !url.includes("http://")){
+    return res.json({ error : "invalide url"})
+  }
+
+  if (foundIndex < 0){
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
     })
   }
-  // If the url has an invalid format
-  catch {
-    res.json({ error: 'invalid url' })
-  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  })
 });
 
 app.get('/api/shorturl/:short_url', (req, res) => {
-  let short_url = req.params.short_url;
-  // Find the original url from the database
-  URLModel.findOne({short_url: short_url}).then((foundURL) => {
-    if (foundURL){
-      let original_url = foundURL.original_url;
-      res.redirect(original_url);
-    } else {
-      res.json({message: "The short url does not exist!"});
-    }
-  });
+  const shorturl = parseInt(req.params.short_url)
+  const foundIndex = shortUrls.indexOf(shorturl)
+
+  if(foundIndex < 0) {
+    return res.json({
+      "error": "No short URL found for the given output"
+    })
+  }
+  res.redirect(originalUrls[foundIndex])
 })
 
 app.listen(port, function() {
